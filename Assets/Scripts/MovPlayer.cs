51b4720df4d503e22b3d3c8e1dccbcd44cf695f4
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-08-16 15:34:50 -0600

    algo hice de codigo , verificar

--- Assets/Scripts/MovPlayer.cs
+++ Assets/Scripts/MovPlayer.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MovPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float movePlayer;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rollingTimer;
+    [SerializeField] private Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+    private Vector2 vector2D;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        movimiento();
+    }
+
+    void movimiento()
+    {
+        // caminar 
+        if (movePlayer == 10){
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+            vector2D = new Vector2(horizontal, vertical);
+            rb.AddForce(vector2D * Time.deltaTime * movePlayer);
+        }
+        // correr
+        if (movePlayer == 25){
+            Input.GetKeyDown(KeyCode.LeftShift);
+        }
     }
 }
