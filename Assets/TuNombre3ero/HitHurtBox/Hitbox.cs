d76273204a01b17020beb93f2255d34c7212147a
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-11-15 15:38:14 -0600

    HAY QUE HACER TAREA

--- Assets/TuNombre3ero/HitHurtBox/Hitbox.cs
+++ Assets/TuNombre3ero/HitHurtBox/Hitbox.cs
@@ -3,19 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace SotomaYorch.DungeonCrawler
 {
-
+    [RequireComponent(typeof(Collider2D))]
     public class Hitbox : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        #region KnobsParameters
+        // public uint damage =1;
+        public float lifetime = 1f;
+        #endregion KnobsParameters
 
+        #region References
+
+        [SerializeField] protected Collider2D _collider;
+
+        #endregion References
+        #region RuntimeVariables
+        bool _isHitBoxActive;
+        #endregion RuntimeVariables
+
+        #region UnityMethods
+
+        private void OnDrawGizmos()
+        {
+#if UNITY_EDITOR
+            if (_collider == null)
+            {
+                _collider = GetComponent<Collider2D>();
+            }
+#endif
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        #endregion UnityMethods
 
+        #region PublicMethods
+        public void ActivateHitBox()
+        {
+            if (!_isHitBoxActive)
+            {
+
+                StartCoroutine(lifeTime());
+            }
         }
+        #region Coroutines
+        public IEnumerator lifeTime()
+        {
+            _isHitBoxActive = true;
+            _collider.enabled = true;
+            yield return new WaitForSeconds(lifetime);
+            _collider.enabled = false;
+            _isHitBoxActive = false;
+        }
+        #endregion Coroutines
+
+        #endregion PublicMethods
     }
 }
--- Assets/TuNombre3ero/HitHurtBox/Hurtbox.cs
+++ Assets/TuNombre3ero/HitHurtBox/Hurtbox.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace SotomaYorch.DungeonCrawler
 {
 
     public class Hurtbox : MonoBehaviour
     {
+        #region KnobsParameters
+
+        public int maxHealthPoints = 3; //vida maxima par amis agentes
+        public float coolDownTime = 1.0f; // damage por segundo "DPS" al HITBOX
+
+        #endregion KnobsParameters
+
+        #region References
+
+        [SerializeField] protected Agent _agent;
+        #endregion References
+
+
         #region RuntimeVaribales
 
         protected bool _isInCooldown;
+        protected int _currentHealthPoints;
 
         #endregion RuntimeVariables
 
         #region UnityMethods
+        private void OnDrawGizmos()
+        {
 
+            if (_agent != null)
+            {
+                transform.parent.gameObject.GetComponent<Agent>()];
+            }
+        }
+        void Start()
+        {
+            _currentHealthPoints = maxHealthPoints;
+        }
         private void OnTriggerStay2D(Collider2D other)
         {
-            if (!_isInCooldown)
+            if (!_isInCooldown)// to be inmune or not :3
             {
                 // i have the potencialñ to be harmed by a hitbox :O
                 if (other.gameObject.CompareTag("Hitbox"))
@@ -26,11 +52,32 @@ namespace SotomaYorch.DungeonCrawler
                     if (other.gameObject.layer != gameObject.layer)
                     {
                         //damn, i am about to be hurt Dx
+                        _currentHealthPoints -= 1; // other.gameObject.GetComponent<Hitbox>().GetDamage;
+
+                        if (_currentHealthPoints <= 0)
+                        {
+                            //so i will die, it is time :,(
+                        }
+                        else
+                        {
+                            StartCoroutine(Cooldown());
+                        }
                     }
                 }
             }
         }
 
         #endregion UnityMethods
+
+        #region Coroutines
+
+        protected IEnumerator Cooldown()
+        {
+            _isInCooldown = true; //to be Inmune for a certain time;
+            yield return new WaitForSeconds(coolDownTime); // to be inmune for some time 
+            _isInCooldown = false;
+        }
+
+        #endregion Coroutines
     }
 }
