36b57ff2a60087b05202d1b55e83097ac5d45f65
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-11-20 16:42:21 -0600

    vb

--- Assets/TuNombre3ero/Code/FSM.cs
+++ Assets/TuNombre3ero/Code/FSM.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FSM : MonoBehaviour {
     public agentFSM fsm;
@@ -48,6 +50,12 @@ public class FSM : MonoBehaviour {
         ATTACK,
         DIE //TODO: Complete the code to administate this new state
     }
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+    void Update() {
+        initializedStatesAgent();
+    }
     public void initializedStatesAgent() {
         switch (fsm) {
 
@@ -55,59 +63,118 @@ public class FSM : MonoBehaviour {
             case agentFSM.IDLE_UP:
             case agentFSM.IDLE_RIGHT:
             case agentFSM.IDLE_LEFT:
-                move();
+                Move();
                 break;
 
             case agentFSM.MOVING_DOWN:
             case agentFSM.MOVING_UP:
             case agentFSM.MOVING_RIGHT:
             case agentFSM.MOVING_LEFT:
-                move();
+                Move();
                 break;
 
             case agentFSM.ATTACKING_DOWN:
             case agentFSM.ATTACKING_UP:
             case agentFSM.ATTACKING_RIGHT:
             case agentFSM.ATTACKING_LEFT:
-                attack();
+                Attack();
                 break;
 
             case agentFSM.SPRINT_DOWN:
             case agentFSM.SPRINT_UP:
             case agentFSM.SPRINT_RIGHT:
             case agentFSM.SPRINT_LEFT:
-                sprint();
+                Sprint();
                 break;
 
             case agentFSM.CARRY_DOWN:
             case agentFSM.CARRY_UP:
             case agentFSM.CARRY_RIGHT:
             case agentFSM.CARRY_LEFT:
-                carry();
+                Carry();
                 break;
 
             case agentFSM.DEATH:
-                death();
+                Death();
                 break;
         }
     }
-    public void idle() {
-
+    public void Idle() {
+        animator.SetBool("STOP", true);
+        animator.SetBool("MOVING_UP", false);
+        animator.SetBool("MOVING_DOWN", false);
+        animator.SetBool("MOVING_LEFT", false);
+        animator.SetBool("MOVING_RIGHT", false);
     }
-    public void move() {
+    public void Move() {
+        animator.SetBool("STOP", false);
+        animator.SetBool("MOVING_UP", fsm == agentFSM.MOVING_UP);
+        animator.SetBool("MOVING_DOWN", fsm == agentFSM.MOVING_DOWN);
+        animator.SetBool("MOVING_LEFT", fsm == agentFSM.MOVING_LEFT);
+        animator.SetBool("MOVING_RIGHT", fsm == agentFSM.MOVING_RIGHT);
 
-    }
-    public void attack() {
+        // Muevoel mono en la dirección que quiero
+        Vector3 direction = Vector3.zero;
 
-    }
-    public void sprint() {
+        if (fsm == agentFSM.MOVING_UP) direction = Vector3.up;
+        else if (fsm == agentFSM.MOVING_DOWN) direction = Vector3.down;
+        else if (fsm == agentFSM.MOVING_LEFT) direction = Vector3.left;
+        else if (fsm == agentFSM.MOVING_RIGHT) direction = Vector3.right;
 
+        transform.position += direction * Time.deltaTime * 3;// a ver si con 3 es suficiente, si no le aumento o resto
     }
-    public void carry() {
 
+    public void Attack() {
+        animator.SetBool("STOP", true);
+        //animator.SetBool("MOVING_UP",fsm == agentFSM.ATTACKING_UP);
+        // PREGUNTAR SI ESTO ESTO ES FACTIBLE 
+        //preguntar al profe como debo incluir el hitbox,chance solo la llamo por referencia script< >()
     }
-    public void death() {
+    public void Sprint() {
+        animator.SetBool("STOP", false);
+        animator.SetBool("MOVING_UP", fsm == agentFSM.SPRINT_UP);
+        animator.SetBool("MOVING_DOWN", fsm == agentFSM.SPRINT_DOWN);
+        animator.SetBool("MOVING_LEFT", fsm == agentFSM.SPRINT_LEFT);
+        animator.SetBool("MOVING_RIGHT", fsm == agentFSM.SPRINT_RIGHT);
 
+
+        Vector3 direction = Vector3.zero;
+
+        if (fsm == agentFSM.SPRINT_UP) direction = Vector3.up;
+        else if (fsm == agentFSM.SPRINT_DOWN) direction = Vector3.down;
+        else if (fsm == agentFSM.SPRINT_LEFT) direction = Vector3.left;
+        else if (fsm == agentFSM.SPRINT_RIGHT) direction = Vector3.right;
+
+        transform.position += direction * Time.deltaTime * 6; //si le subo al move le saco el doble a este
     }
+    public void Carry() {
+        animator.SetBool("Stop", true);
+        //poner logica de cargar el objeto igual con una condicional if e input 
+        // pregunta si se puede usar el system input para esto 
+    }
+    public void Death() {
+        animator.SetBool("Stop", true);
+        // activar el panel de muerte 
+        //  SceneManager.SetActiveScene("PanelPausa");
+        this.enabled = false; // Detiene el script.
+    }
+    private void ResetAnimatorParameters() {
+        //ME AYUDA PARA BORRAR LAS ANIMACIONES DESPUES D EUNA EJECUCION 
+        //ASI NO HAY PROBLEMAS DE QUE LA WEA QUIERA MOSTRARME MAS DE 2 ANIMACIONES CUANDO EL USUARIO
+        // ESTE USANDO DIFERENTES INPUTS QUE REQUIERA CADA UNA, 1 ANIMACION
+        animator.SetBool("Stop", false);
+        animator.SetBool("Move_Up", false);
+        animator.SetBool("Move_Down", false);
+        animator.SetBool("Move_Left", false);
+        animator.SetBool("Move_Right", false);
+    }
+    public void ChangeState(agentFSM newAgentState) {
+        ResetAnimatorParameters();
+        fsm = newAgentState; // Actualiza el estado actual de la FSM.
+        initializedStatesAgent();
+        // Ejecuta la lógica específica del nuevo estado.
+        //ponerlo en mi update
+    }
+
 }
 
